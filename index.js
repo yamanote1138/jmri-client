4910908996f4b566627de5568cdf910a9745732e
Chad Francis <discgolfer1138@gmail.com>
2013-03-24 00:51:15 -0600

    got basic functionality working

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-index.js
+var jmri = require('./lib/jmri.js'),
+	prompt = require('prompt');
+
+
+prompt.start();
+
+prompt.get(['host'], function (err, result) {
+    if (err) { console.log(err); return; }
+	
+	var jmriClient = new jmri({
+		host: result.host
+	});
+
+	jmriClient.getThrottle(11, function(err, data){
+		if(!err) console.log(data);
+	});
+});
