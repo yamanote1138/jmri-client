bcd3e83888f4d1b358db632f6dde59a9814e2a38
Chad Francis <yamanote1138@gmail.com>
2022-09-07 01:30:14 -0600

    started to implement communication with jmri json interface - many runtime errors, can't test yet

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 'use strict';
 
 import fetch from "node-fetch";
+import { RequestInit } from "node-fetch";
 // const xml2json = require('xml2json');
 
 type Protocol = 'http' | 'https';
 type FFunction = 'F1' | 'F2' | 'F3' | 'F4' | 'F5' | 'F6' | 'F7' | 'F8' | 'F9' | 'F10' | 'F11' | 'F12'; 
 
+
 class JmriClient {
   protected readonly _protocol:Protocol;
   protected readonly _host:string;
-  protected readonly _path:string;
   protected readonly _port:number = 80;
 
-  constructor(protocol: Protocol, host: string, path: string, port?: number) {
+  constructor(protocol: Protocol, host: string, port?: number) {
     this._protocol = protocol;
     this._host = host;
-    this._path = path;
     if (port !== undefined) this._port = port;
 
     this._validate();
@@ -24,36 +24,43 @@ class JmriClient {
   protected _validate = () => {
     if(!this._protocol) throw new Error('protocol is empty');
     if(!this._host) throw new Error('host is empty');
-    if(!this._path) throw new Error('path is empty');
     if(this._port < 0 || this._port > 65535) throw new Error('port is out of range');
   };
 
-  protected _send = async (xmldata:string) => {
-    const body = `<?xml version="1.0" encoding="UTF-8"?><XMLIO>${xmldata}</XMLIO>`;
-    const uri = `${this._protocol}://${this._host}/${this._path}`;
-    return await fetch(
-      uri,
-      {
-        method: 'PUT',
-        body: JSON.stringify(body),
-        headers: {
-          'Content-Type': 'application/xml'
-        }
+  protected _getValue = async (type:string) => {
+    return await this._send('GET', type);
+  }
+
+  protected _setValue = async (type:string, data:object) => {
+    return await this._send('POST', type, data);
+  }
+
+  protected _send = async (method:string, type:string, data?:object) => {
+    const uri = `${this._protocol}://${this._host}:${this._port}/json/${type}`;
+    const options:RequestInit = {
+      method: method
+    }
+    if(data !== undefined){
+      options.body = JSON.stringify(data),
+      options.headers = {
+        'Content-Type': 'application/json; charset=utf-8'
       }
-    );
-    // if (body) body = JSON.parse(xml2json.toJson(body));
+    }
+
+    return await fetch(uri, options);
   }
 
   getPower = async () => {
-    let xmldata = '';
-    return await this._send('<item><type>power</type><name>power</name></item>');
+    return await this._getValue('power');
   }
 
-  setPower = async (value:string) => {
-    let xmldata = `<item><type>power</type><name>power</name><set>${value}</set></item>`;
-    return await this._send(xmldata);
+  setPower = async (isOn:boolean) => {
+    return await this._setValue('power', {
+      value: (isOn) ? 2:4
+    });
   }
 
+  /*
   getThrottle = async (addresses:Array<string>) => {
     let xmldata = '';
     addresses.forEach(function(address){
@@ -86,6 +93,7 @@ class JmriClient {
     let xmldata = '<list type="turnout" />';
     return await this._send(xmldata);
   }
+  */
   
 }
 
--- tests/unit/index.spec.ts
+++ tests/unit/index.spec.ts
@@ -5,26 +5,20 @@ import { JmriClient } from '../../src/index'
 describe('Constructor', () => {
   describe('when well-formed parameters are given', () => {
     it('should not throw an error', () => {
-      expect(() => {new JmriClient('http', '192.168.1.138', 'xmlio');}).not.toThrowError();
-      expect(() => {new JmriClient('http', '192.168.1.138', 'xmlio', 1138);}).not.toThrowError();
+      expect(() => {new JmriClient('http', '192.168.1.138');}).not.toThrowError();
+      expect(() => {new JmriClient('http', '192.168.1.138', 1138);}).not.toThrowError();
     });
   });
   describe('when the host is empty', () => {
     it('should throw an error', () => {
-      expect(() => {new JmriClient('http', '', 'xmlio');}).toThrow('host is empty');
-      expect(() => {new JmriClient('http', '', 'xmlio', 1138);}).toThrow('host is empty');
-    });
-  });
-  describe('when path is empty', () => {
-    it('should throw an error', () => {
-      expect(() => {new JmriClient('http', '192.168.1.138', '');}).toThrow('path is empty');
-      expect(() => {new JmriClient('http', '192.168.1.138', '', 1138);}).toThrow('path is empty');
+      expect(() => {new JmriClient('http', '');}).toThrow('host is empty');
+      expect(() => {new JmriClient('http', '', 1138);}).toThrow('host is empty');
     });
   });
   describe('when port is out of range', () => {
     it('should throw an error', () => {
-      expect(() => {new JmriClient('http', '192.168.1.138', 'xmlio', -1);}).toThrow('port is out of range');
-      expect(() => {new JmriClient('http', '192.168.1.138', 'xmlio', 65536);}).toThrow('port is out of range');
+      expect(() => {new JmriClient('http', '192.168.1.138', -1);}).toThrow('port is out of range');
+      expect(() => {new JmriClient('http', '192.168.1.138', 65536);}).toThrow('port is out of range');
     });
   });
 });
