e8321468f4fae07e066cff63247d408a9fcec3bd
Chad Francis <yamanote1138@gmail.com>
2022-09-15 22:22:27 -0600

    updated deps; added prompt to functional test

--- tests/functional/demo.mjs
+++ tests/functional/demo.mjs
@@ -1,8 +1,31 @@
 "use strict";
-
+import prompt from "prompt";
 import { JmriClient } from "../../dist/index.js";
 
-const client = new JmriClient('http', 'jmri.local', 12080);
+prompt.start();
+
+	const prompt_options = [
+		{
+			'name':'protocol',
+			'default':'http'
+		},
+		{
+			'name':'host',
+			'default':'jmri.local'
+		},
+		{
+			'name':'port',
+			'default':12080
+		}
+	];
+
+	const { protocol, host, port } = await prompt.get(prompt_options);
+
+	console.log(`protocol set to ${protocol}`);
+	console.log(`host set to ${host}`);
+	console.log(`port set to ${port}`);
+
+const client = new JmriClient(protocol, host, port);
 
 client.getPower().then((response)=>{
   console.log(response.data);
